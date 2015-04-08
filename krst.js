dbd66e142332e5cf34acbedcee760d41a7b9dc81
Hampus Forsvall <forzvall@gmail.com>
2015-04-08 16:05:53 +0200

    Atleast it shows the dishes, a small step for man

--- krst.js
+++ krst.js
@@ -1,5 +1,15 @@
 var gfeed = require('google-feed-api');
-var feed = new gfeed.Feed('http://cm.lskitchen.se/johanneberg/karrestaurangen/sv/2015-04-08.rss');
+var today = new Date().toISOString().slice(0,10)
+var feed = new gfeed.Feed('http://cm.lskitchen.se/johanneberg/karrestaurangen/sv/'+today+'.rss');
+
+
 feed.listItems(function(items){
-    console.log(items);
+    console.log("=== Classic Kött ===");
+    console.log(items[0].contentSnippet);
+
+    console.log("=== Classic Fisk ===");
+    console.log(items[2].contentSnippet);
+
+    console.log("=== Soppa ===");
+    console.log(items[3].contentSnippet);
 });
