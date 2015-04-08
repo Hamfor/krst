849cf7c37c363886dd61b6dc8a626bba477995e0
Hampus Forsvall <forzvall@gmail.com>
2015-04-08 15:43:58 +0200

    deleted: thrash

--- krst.s
+++
@@ -1,5 +0,0 @@
-var gfeed = require('google-feed-api');
-var feed = new gfeed.Feed('http://cm.lskitchen.se/johanneberg/karrestaurangen/sv/2015-04-08.rss');
-feed.listItems(function(items){
-    console.log(items);
-});
