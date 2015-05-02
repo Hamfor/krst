a9a3d75a3658c904de83d463b490986f1db03d76
Hampus Forsvall <forzvall@gmail.com>
2015-05-02 18:48:29 +0200

    Semiworking...

--- krst.js
+++ krst.js
@@ -1,15 +1,19 @@
+#!/usr/bin/env node
+
 var feedread = require('feed-read');
 var today = new Date().toISOString().slice(0,10)
 var feed = "http://cm.lskitchen.se/johanneberg/karrestaurangen/sv/"+today+".rss";
 
-feedread(feed, function(err, articles){
-	var prev = "first"; 
-	for(var i = 0; i < articles.length; i++){
-		if(articles[i].title !== prev || prev === "first"){
-			console.log("=== " + articles[i].title + " ===");
-			console.log(articles[i].content.slice(0,articles[i].content.indexOf("@")))
+(function(){
+	feedread(feed, function(err, articles){
+		var prev = "first"; 
+		for(var i = 0; i < articles.length; i++){
+			if(articles[i].title !== prev || prev === "first"){
+				console.log("=== " + articles[i].title + " ===");
+				console.log(articles[i].content.slice(0,articles[i].content.indexOf("@")))
+			}
+			//Skips duplicates, i.e. english translations
+			prev = articles[i].title;
 		}
-		//Skips duplicates, i.e. english translations
-		prev = articles[i].title;
-	}
-});
+	});
+})();
